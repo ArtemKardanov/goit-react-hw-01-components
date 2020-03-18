d25304654da62f102af457e08ae7ee2fdba8d766
Your Name <rtkard90@gmail.com>
2020-03-18 15:15:21 +0200

    Updates

---
+++ precache-manifest.164a3f54555f15335f8e3e1fadc27de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebd033a8f6cb5bfc6dc7b93ec613bed",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "33d3d37952f0226a13d6",
+    "url": "/goit-react-hw-01-components/static/css/main.66b27dd6.chunk.css"
+  },
+  {
+    "revision": "00087a74acc0cb44d5cd",
+    "url": "/goit-react-hw-01-components/static/js/2.c8eed75f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.c8eed75f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d3d37952f0226a13d6",
+    "url": "/goit-react-hw-01-components/static/js/main.95a654f7.chunk.js"
+  },
+  {
+    "revision": "5e28ef9210cdb0f50653",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.47b9d3ea.js"
+  }
+]);
